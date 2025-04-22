969e450551f84134adb492d69d1dc2b6b495fdee
Kale-Ko <github@kaleko.dev>
2025-04-22 14:33:32 -0400

    Initial version, just needs a better user output

---
+++ src/main.ts
@@ -0,0 +1,274 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const VERSION = "1.0.0";
+const USER_AGENT: string = `Bun/${Bun.version} ms-asset-downloader/${VERSION}`;
+const HOME_URL: string = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
+const ASSET_URL: string = "https://resources.download.minecraft.net"
+
+const CACHE_DIRECTORY = path.resolve("cache/");
+const PISTON_CACHE_DIRECTORY = path.join(CACHE_DIRECTORY, "piston/");
+const ASSETS_CACHE_DIRECTORY = path.join(CACHE_DIRECTORY, "assets/");
+
+fs.mkdirSync(CACHE_DIRECTORY, { recursive: true });
+fs.mkdirSync(PISTON_CACHE_DIRECTORY, { recursive: true });
+fs.mkdirSync(ASSETS_CACHE_DIRECTORY, { recursive: true });
+
+interface CachedResponse<T> {
+    cached: boolean
+    value: T
+}
+
+interface VersionList {
+    latest: {
+        release: string
+        snapshot: string
+    }
+    versions: {
+        id: string
+        type: "release" | "snapshot" | "old_snapshot" | "old_beta" | "old_alpha" | "experiment"
+        url: string
+        time: string
+        releaseTime: string
+        sha1: string
+        complianceLevel?: 0 | 1
+    }[]
+}
+
+interface Version {
+    id: string
+    type: "release" | "snapshot" | "old_snapshot" | "old_beta" | "old_alpha" | "experiment"
+    time: string
+    releaseTime: string
+    /**
+     * Present on all versions except 13w24a-13w38c (1.6-1.6.3)
+    */
+    complianceLevel?: 0 | 1
+    mainClass: string
+    minimumLauncherVersion: number
+    /**
+     * Present on all versions except 13w24a-13w38c (1.6-1.6.3)
+    */
+    javaVersion?: {
+        component: string
+        majorVersion: number
+    }
+    arguments?: {
+        [key: string]: (string | {
+            value: string | string[]
+            rules?: any[] // TODO
+        })[]
+    }
+    minecraftArguments?: string
+    downloads: {
+        [key: string]: {
+            sha1: string
+            size: number
+            url: string
+        }
+    }
+    libraries: {
+        name: string
+        downloads: {
+            classifiers?: {
+                [key: string]: {
+                    path: string
+                    sha1: string
+                    size: number
+                    url: string
+                }
+            }
+        } & {
+            [key: string]: {
+                path: string
+                sha1: string
+                size: number
+                url: string
+            }
+        }
+        extract?: any // TODO
+        natives?: {
+            [key: string]: string
+        }
+        rules?: any[] // TODO
+    }[]
+    logging?: {
+        [key: string]: {
+            type: string
+            argument: string
+            file: {
+                id: string
+                sha1: string
+                size: number
+                url: string
+            }
+        }
+    }
+    assets: string
+    assetIndex: {
+        id: string
+        sha1: string
+        size: number
+        totalSize: number
+        url: string
+    }
+}
+
+interface AssetIndex {
+    /**
+     * Only present on 13w23b and below. I have no idea what this means.
+     */
+    map_to_resources?: boolean
+    /**
+     * Only present on 13w24a-13w48b and below. I have no idea what this means.
+     */
+    virtual?: boolean
+    objects: {
+        [key: string]: {
+            hash: string
+            size: number
+        }
+    }
+}
+
+async function downloadVersionList(): Promise<CachedResponse<VersionList>> {
+    let filePath = path.join(PISTON_CACHE_DIRECTORY, "version_manifest_v2.json");
+    if (fs.existsSync(filePath) && Date.now() - fs.statSync(filePath).mtime.getTime() < 1000 * 60 * 30) {
+        let data = fs.readFileSync(filePath, { encoding: "utf8" });
+
+        return { cached: true, value: JSON.parse(data) as VersionList };
+    } else {
+        let response = await Bun.fetch(HOME_URL, {
+            headers: {
+                "Accept": "application/json",
+                "User-Agent": USER_AGENT
+            }
+        });
+
+        if (response.ok) {
+            let data = await response.text();
+
+            fs.mkdirSync(path.dirname(filePath), { recursive: true });
+            fs.writeFileSync(filePath, data, { encoding: "utf8" });
+
+            return { cached: false, value: JSON.parse(data) as VersionList };
+        } else {
+            throw Error(`Response from "${HOME_URL}" was "${response.status} ${response.statusText}"!`);
+        }
+    }
+}
+
+async function downloadVersion(versionId: string): Promise<CachedResponse<Version>> {
+    let versionList = (await downloadVersionList()).value;
+
+    let versionInfo = versionList.versions.find(version => version.id == versionId);
+    if (versionInfo == undefined) {
+        throw Error(`Could not find version "${versionId}"!`);
+    }
+
+    let filePath = path.join(PISTON_CACHE_DIRECTORY, versionInfo.sha1.substring(0, 2), versionInfo.sha1.substring(2));
+    if (fs.existsSync(filePath)) {
+        let data = fs.readFileSync(filePath, { encoding: "utf8" });
+
+        return { cached: true, value: JSON.parse(data) as Version };
+    } else {
+        let response = await Bun.fetch(versionInfo.url, {
+            headers: {
+                "Accept": "application/json",
+                "User-Agent": USER_AGENT
+            }
+        });
+
+        if (response.ok) {
+            let data = await response.text();
+
+            fs.mkdirSync(path.dirname(filePath), { recursive: true });
+            fs.writeFileSync(filePath, data, { encoding: "utf8" });
+
+            return { cached: false, value: JSON.parse(data) as Version };
+        } else {
+            throw Error(`Response from "${versionInfo.url}" was "${response.status} ${response.statusText}"!`);
+        }
+    }
+}
+
+async function downloadAssetIndex(versionId: string): Promise<CachedResponse<AssetIndex>> {
+    let versionList = (await downloadVersionList()).value;
+
+    let versionInfo = versionList.versions.find(version => version.id == versionId);
+    if (versionInfo == undefined) {
+        throw Error(`Could not find version "${versionId}"!`);
+    }
+
+    let version = (await downloadVersion(versionInfo.id)).value;
+
+    let assetIndex = version.assetIndex;
+
+    let filePath = path.join(PISTON_CACHE_DIRECTORY, assetIndex.sha1.substring(0, 2), assetIndex.sha1.substring(2));
+    if (fs.existsSync(filePath)) {
+        let data = fs.readFileSync(filePath, { encoding: "utf8" });
+
+        return { cached: true, value: JSON.parse(data) as AssetIndex };
+    } else {
+        let response = await Bun.fetch(assetIndex.url, {
+            headers: {
+                "Accept": "application/json",
+                "User-Agent": USER_AGENT
+            }
+        });
+
+        if (response.ok) {
+            let data = await response.text();
+
+            fs.mkdirSync(path.dirname(filePath), { recursive: true });
+            fs.writeFileSync(filePath, data, { encoding: "utf8" });
+
+            return { cached: false, value: JSON.parse(data) as AssetIndex };
+        } else {
+            throw Error(`Response from "${assetIndex.url}" was "${response.status} ${response.statusText}"!`);
+        }
+    }
+}
+
+async function downloadAsset(versionId: string, assetPath: string): Promise<CachedResponse<Uint8Array>> {
+    let versionList = (await downloadVersionList()).value;
+
+    let versionInfo = versionList.versions.find(version => version.id == versionId);
+    if (versionInfo == undefined) {
+        throw Error(`Could not find version "${versionId}"!`);
+    }
+
+    let assetIndex = (await downloadAssetIndex(versionInfo.id)).value;
+    let asset = assetIndex.objects[assetPath];
+    if (asset == undefined) {
+        throw Error(`Asset "${assetPath}" does not exist on version ${versionId}!`)
+    }
+
+    let assetUrl = `${ASSET_URL}/${asset.hash.substring(0, 2)}/${asset.hash}`
+
+    let filePath = path.join(ASSETS_CACHE_DIRECTORY, asset.hash.substring(0, 2), asset.hash.substring(2));
+    if (fs.existsSync(filePath)) {
+        let data = fs.readFileSync(filePath);
+
+        return { cached: true, value: data };
+    } else {
+        let response = await Bun.fetch(assetUrl, {
+            headers: {
+                "User-Agent": USER_AGENT
+            }
+        });
+
+        if (response.ok) {
+            let data = await response.bytes();
+
+            fs.mkdirSync(path.dirname(filePath), { recursive: true });
+            fs.writeFileSync(filePath, data);
+
+            return { cached: false, value: data };
+        } else {
+            throw Error(`Response from "${assetUrl}" was "${response.status} ${response.statusText}"!`);
+        }
+    }
+}
+
+export { VERSION, downloadVersionList, downloadVersion, downloadAssetIndex, downloadAsset };
---
+++ src/test.ts
@@ -0,0 +1,143 @@
+import * as main from "./main.ts"
+import { z } from "zod";
+
+const VersionListSchema = z.strictObject({
+    latest: z.object({
+        release: z.string(),
+        snapshot: z.string(),
+    }),
+    versions: z.array(z.object({
+        id: z.string(),
+        type: z.enum(["release", "snapshot", "old_snapshot", "old_beta", "old_alpha", "experiment"]),
+        url: z.string().url(),
+        time: z.string(),
+        releaseTime: z.string(),
+        sha1: z.string(),
+        complianceLevel: z.optional(z.union([z.literal(0), z.literal(1)])),
+    })),
+});
+
+const VersionSchema = z.strictObject({
+    id: z.string(),
+    type: z.enum(["release", "snapshot", "old_snapshot", "old_beta", "old_alpha", "experiment"]),
+    time: z.string(),
+    releaseTime: z.string(),
+    complianceLevel: z.optional(z.union([z.literal(0), z.literal(1)])),
+    mainClass: z.string(),
+    minimumLauncherVersion: z.number().int().positive(),
+    javaVersion: z.optional(z.object({
+        component: z.string(),
+        majorVersion: z.number().int().positive(),
+    })),
+    arguments: z.optional(z.record(
+        z.array(z.union([
+            z.string(),
+            z.object({
+                value: z.union([z.string(), z.array(z.string())]),
+                rules: z.optional(z.array(z.any())),
+            })
+        ]))
+    )),
+    minecraftArguments: z.optional(z.string()),
+    downloads: z.record(z.object({
+        sha1: z.string(),
+        size: z.number().int().positive(),
+        url: z.string().url(),
+    })),
+    libraries: z.array(z.object({
+        name: z.string(),
+        downloads: z.record(z.object({
+            path: z.string(),
+            sha1: z.string(),
+            size: z.number().int().positive(),
+            url: z.string().url(),
+        })).or(z.object({
+            classifiers: z.optional(z.record(z.object({
+                path: z.string(),
+                sha1: z.string(),
+                size: z.number().int().positive(),
+                url: z.string().url(),
+            })))
+        })),
+        extract: z.optional(z.any()),
+        natives: z.optional(z.record(z.string())),
+        rules: z.optional(z.array(z.any())),
+    })),
+    logging: z.optional(z.record(z.object({
+        type: z.string(),
+        argument: z.string(),
+        file: z.object({
+            id: z.string(),
+            sha1: z.string(),
+            size: z.number().int().positive(),
+            url: z.string().url(),
+        }),
+    }))),
+    assets: z.string(),
+    assetIndex: z.object({
+        id: z.string(),
+        sha1: z.string(),
+        size: z.number().int().positive(),
+        totalSize: z.number().int().positive(),
+        url: z.string().url(),
+    }),
+});
+
+const AssetIndexSchema = z.strictObject({
+    map_to_resources: z.optional(z.boolean()),
+    virtual: z.optional(z.boolean()),
+    objects: z.record(z.object({
+        hash: z.string(),
+        size: z.number().int().positive()
+    })),
+});
+
+function tryValidate(object: any, schema: z.ZodObject<any>, info: any) {
+    try {
+        schema.parse(object);
+    } catch (e: any) {
+        if (e instanceof z.ZodError) {
+            for (let issue of e.issues) {
+                if ("unionErrors" in issue) {
+                    for (let e2 of issue.unionErrors) {
+                        for (let subIssue of e2.issues) {
+                            console.warn({ ...info, ...subIssue });
+
+                            let p: any = object;
+                            for (let path of subIssue.path) {
+                                p = p[path];
+                            }
+                            console.log(p);
+                        }
+                    }
+                } else {
+                    console.warn({ ...info, ...issue });
+
+                    let p: any = object;
+                    for (let path of issue.path) {
+                        p = p[path];
+                    }
+                    console.log(p);
+                }
+            }
+        }
+    }
+}
+
+(async () => {
+    let versionList = await main.downloadVersionList();
+
+    tryValidate(versionList, VersionListSchema, { "_file": "version_manifest_v2.json" });
+
+    for (let versionInfo of versionList.versions) {
+        let version = await main.downloadVersion(versionInfo.id);
+
+        tryValidate(version, VersionSchema, { "_version": versionInfo.id, "_file": versionInfo.sha1 });
+
+        {
+            let assetIndex = await main.downloadAssetIndex(versionInfo.id);
+
+            tryValidate(assetIndex, AssetIndexSchema, { "_version": versionInfo.id, "_assetIndex": version.assetIndex.id, "_file": version.assetIndex.sha1 });
+        }
+    }
+})();
